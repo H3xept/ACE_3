a21658fb1974675ea661bb5d36be7bc04afd1cd9
EwanSkene <EwanSkene@users.noreply.github.com>
2018-11-12 19:02:54 +0000

    updated ALU header
    added methods

--- oop/ALU.h
+++ oop/ALU.h
@@ -20,6 +20,14 @@ extern const void * ALU_Class_Descriptor;
 // ...
 
 // Public instance methods for ALU
-// ...
+bool SKC(int16_t num);
+int16_t ADD(int16_t num_1, int16_t num_2);
+int16_t MUL(int16_t num_1, int16_t num_2);
+int16_t DIV(int16_t num_1, int16_t num_2);
+int16_t AND(int16_t num_1, int16_t num_2);
+int16_t OR(int16_t num_1, int16_t num_2);
+int16_t NOT(int16_t num);
+int16_t SHL(int16_t num_1, int16_t num_2);
+int16_t SHR(int16_t num_1, int16_t num_2);
 
 #endif
