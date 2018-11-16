3bdb5e45859c1a552bdeb35df679bfc3d160de43
H3xept <h3xept@gmail.com>
2018-11-16 17:24:03 +0000

    fix(Flag Delegate)

--- oop/protocols/FlagDelegate.h
+++ oop/protocols/FlagDelegate.h
@@ -2,6 +2,7 @@
 #define __FLAGDELEGATE__
 
 #include <ctype.h>
+#include "../constants/var_word_size.h"
 
 typedef enum {
 	k_Status_Flag_Halt,
@@ -10,8 +11,10 @@ typedef enum {
 	k_Status_Flag_Exit_Codes
 } k_Status_Flag;
 
-typedef struct {
-	void 	(*FlagDelegate_Set_Flag)(FlagDelegate * delegate, k_Status_Flag flag, uint8_t value);
-} FlagDelegate;
+struct FlagDelegate{
+	void 	(*FlagDelegate_Set_Flag)(struct FlagDelegate * delegate, k_Status_Flag flag, uint8_t value);
+	uword_t (*FlagDelegate_Get_Flags_As_Word)(struct FlagDelegate * delegate);
+	uint8_t (*FlagDelegate_Read_Flag)(struct FlagDelegate * delegate, k_Status_Flag flag);
+};
 
 #endif
